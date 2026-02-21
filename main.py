d1bc2d1e9c410e0390d008668783017f1c7db5d3
Govind Jangid <govindjangid75@gmail.com>
2026-02-21 17:55:42 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# def main():
+#     print("Hello from chai-streamlit!")
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+import streamlit as st
+
+st.title("Chai Streamlit")
+st.subheader("Hello from chai-streamlit!")
+st.text("hello text from chai-streamlit")
+st.write("hello write from chai-streamlit")
